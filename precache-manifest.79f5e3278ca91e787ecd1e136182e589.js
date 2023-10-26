58315e0e04b8f4b9ec18f6b8b8488f8928c9ac21
aneth arias <Aneth Arias>
2023-10-26 13:32:28 -0600

    Updates

---
+++ precache-manifest.79f5e3278ca91e787ecd1e136182e589.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "10da461f0b7e28a2d2de",
+    "url": "/portafolio-aneth/static/css/main.79c4c6d9.chunk.css"
+  },
+  {
+    "revision": "10da461f0b7e28a2d2de",
+    "url": "/portafolio-aneth/static/js/main.10da461f.chunk.js"
+  },
+  {
+    "revision": "bd78a5334beda9fcdcb7",
+    "url": "/portafolio-aneth/static/js/1.bd78a533.chunk.js"
+  },
+  {
+    "revision": "d4293925620a063af82d",
+    "url": "/portafolio-aneth/static/css/2.1ac52486.chunk.css"
+  },
+  {
+    "revision": "d4293925620a063af82d",
+    "url": "/portafolio-aneth/static/js/2.d4293925.chunk.js"
+  },
+  {
+    "revision": "a50089521fddb5547dfb",
+    "url": "/portafolio-aneth/static/js/runtime~main.a5008952.js"
+  },
+  {
+    "revision": "d088d93cf0b893d29baf0993ef52c663",
+    "url": "/portafolio-aneth/static/media/logo.d088d93c.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/portafolio-aneth/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/portafolio-aneth/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/portafolio-aneth/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/portafolio-aneth/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "bfe6d1a6d579d73c0c625ec0afe2855b",
+    "url": "/portafolio-aneth/static/media/header-img.bfe6d1a6.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/portafolio-aneth/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/portafolio-aneth/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/portafolio-aneth/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "ad4b60c724d13124ac6bf55ab9af8e30",
+    "url": "/portafolio-aneth/static/media/project1.ad4b60c7.png"
+  },
+  {
+    "revision": "b0035527fafb1b92529bfa15271dc6b3",
+    "url": "/portafolio-aneth/static/media/project2.b0035527.png"
+  },
+  {
+    "revision": "878e714fc32dc5b33f90489161415dc9",
+    "url": "/portafolio-aneth/static/media/project3.878e714f.png"
+  },
+  {
+    "revision": "2b8ee525369be8099c0061aa7a562565",
+    "url": "/portafolio-aneth/static/media/project4.2b8ee525.png"
+  },
+  {
+    "revision": "a8a69ca6e061530ab808894b235a46e7",
+    "url": "/portafolio-aneth/static/media/project5.a8a69ca6.png"
+  },
+  {
+    "revision": "ea37c0b00718985d4903863a8ee6bdce",
+    "url": "/portafolio-aneth/static/media/project6.ea37c0b0.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/portafolio-aneth/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/portafolio-aneth/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/portafolio-aneth/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/portafolio-aneth/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/portafolio-aneth/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/portafolio-aneth/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "5f3ca49706055342e92708c7dd31849f",
+    "url": "/portafolio-aneth/static/media/banner-bg.5f3ca497.png"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/portafolio-aneth/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/portafolio-aneth/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/portafolio-aneth/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/portafolio-aneth/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/portafolio-aneth/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/portafolio-aneth/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "08e5b04289e11fe1c0776cef35c4bcf2",
+    "url": "/portafolio-aneth/static/media/project-img3.08e5b042.png"
+  },
+  {
+    "revision": "504224d81c4e933e7cbfb141880dc023",
+    "url": "/portafolio-aneth/static/media/coru2.504224d8.png"
+  },
+  {
+    "revision": "71576f5ad0d6afb091507b01c713ed80",
+    "url": "/portafolio-aneth/index.html"
+  }
+];
