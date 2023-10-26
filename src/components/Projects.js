7c8f1c1b5ea6ea3a4a08d08d0dade8a1aac97923
aneth arias <Aneth Arias>
2023-10-26 11:54:02 -0600

    portafolio

---
+++ src/components/Projects.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import project1 from "../assets/img/project1.png";
+import project2 from "../assets/img/project2.png";
+import project3 from "../assets/img/project3.png";
+import project4 from "../assets/img/project4.png";
+import project5 from "../assets/img/project5.png";
+import project6 from "../assets/img/project6.png";
+import projImg3 from "../assets/img/project-img3.png";
+import coru2 from "../assets/img/coru2.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Página web",
+      description: "inicio pagina pec solutions",
+      imgUrl:  project2,
+    },
+    {
+      title: "Diseño",
+      description: "diseño de la página",
+      imgUrl: project1,
+    },
+    {
+      title: "Iconos",
+      description: "iconos de la página",
+      imgUrl: project3,
+    },
+    {
+      title: "Coru",
+      description: "perfil creado con boot",
+      imgUrl:  project4,
+    },
+    {
+      title: "Coru",
+      description: "login financiero conectado a BD",
+      imgUrl: project5,
+    },
+    {
+      title: "Coru",
+      description: "animaciones en react con modal",
+      imgUrl: project6,
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Proyectos</h2>
+                <p>a lo largo de mi carrera he realizado diferentes proyectos para empresas como coru, isiditmex, y un portal educativo en chile</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                   
+                   
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
